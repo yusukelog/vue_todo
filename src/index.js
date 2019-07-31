12f3d64dd18ebeec3f5ec7398c7bec243de0e4ab
koga <koga-y@gigno.co.jp>
2019-07-31 18:59:00 +0900

    install bootstrap

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import Vue from 'vue'
+// Bootstrapのスタイルシート側の機能を読み込む
+import "bootstrap/dist/css/bootstrap.min.css"
+// BootstrapのJavaScript側の機能を読み込む
+import "bootstrap"
 
 new Vue({
     el:'#app',
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,15 @@
 module.exports = {
+    module:{
+      rules:[
+        {
+          test:/\.css$/,
+          use:[
+            "style-loader",
+            "css-loader"
+          ]
+        }
+      ]
+    },
     resolve: {
       alias: {
         'vue$': 'vue/dist/vue.esm.js'
